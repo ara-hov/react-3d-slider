65bdff8567f0b9a0207c21dc1e4d1fbb0108e400
ara-hov <ara90hovhannisyan@gmail.com>
2021-04-29 12:23:37 +0400

    added slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @ts-ignore
+import ImageSlider from './components/image-slider/ImageSlider';
+import './App.scss';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className='App'>
+      <div className='app-title'>
+        <h1>WHAT WE DO</h1>
+        <p className='app-info'>
+          We develop successful apps for our clients who range from startup
+          entrepreneurs to Fortune 500s
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <ImageSlider />
+      <Button variant='contained'>
+        <span>View all projects</span>
+      </Button>
     </div>
   );
 }
